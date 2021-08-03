e83b299bf8d29e1c65fdc0a9d19596955f7739c5
João Chaminé <joao.chamine@multicert.com>
2021-08-03 11:43:47 +0100

    Removing hashing the password due to the password is one of the user choice.

--- Scrypt.py
+++ Scrypt.py
@@ -13,8 +13,6 @@ import platform
 from res.cryptography import Encryption
 
 # Vars
-# HASH = "NVZNfGwjZGhJelQ0JFpVejlVMUIK"
-HASH = ""
 SYSTEM = platform.system()
 
 
@@ -29,22 +27,13 @@ def main():
     mode = options.method
     mode.lower()
     folder = options.folder
-    IN_PASS = options.password
-
-    IN_PASS_bytes = IN_PASS.encode("ascii")
-    IN_PASS_base64_bytes = base64.b64encode(IN_PASS_bytes)
-    HASH = IN_PASS_base64_bytes.decode("ascii")
+    PASS = options.password
 
     if SYSTEM.lower() == "windows":
         folders = folder.replace("\\", "\\\\")
         folders_to_encrypt = [f"{folders}"]
     else:
         folders_to_encrypt = [f"{folder}"]
-
-    # Decode Password
-    password_encode = HASH.encode('ascii')
-    pass_decode = base64.b64decode(password_encode)
-    PASS = pass_decode.decode("ascii")
 
     if mode == "encrypt":
         crypt = Encryption(folders_to_encrypt, PASS, SYSTEM)
